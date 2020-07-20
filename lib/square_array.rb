75d4d78d14a35ac71af51002745a7c03ef1e2de4
artificial-composition-7121 <tbutler1132@gmail.com>
2020-07-20 18:26:18 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,6 +1,6 @@
 def square_array(array)
   counter = 0
-  while counter = array.length do
+  while counter < array.length do
     array.shift
     array[counter] = array[counter] ** 2
     counter += 1
